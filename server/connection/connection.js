bf61461f12bfde4fe61d139559eb543423f8982c
Prathik2004 <prathiksenprathapan@gmail.com>
2025-05-28 17:28:41 +0530

    start

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -4,7 +4,6 @@ require('dotenv').config();  // to load .env variables
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
     });
     console.log('MongoDB connected');
   } catch (err) {
