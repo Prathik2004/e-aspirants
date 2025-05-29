c66defef5d7331a77d72477a4ba222076f798fa0
Prathik2004 <prathiksenprathapan@gmail.com>
2025-05-29 21:08:37 +0530

    Path change

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.post('/api/sell-book', upload.single('productPhoto'), async (req, res) => {
     }
 
     // Normalize path and set to bookData
-    const normalizedPath = '/' + req.file.path.replace(/\\/g, '/'); // Ensures Unix-style and starts with '/'
+    const normalizedPath = '/uploads/' + req.file.filename; // use forward slashes for web
     bookData.productPhoto = normalizedPath;
 
     const newBook = new Booklisting(bookData);
